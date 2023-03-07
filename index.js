29d5c489cd98e849bcd33385f590a70f3cb2d625
rossivk <rosica.kirkova@gmail.com>
2023-03-07 14:02:14 +0000

    navbar and top section
    reduced the code for navbar switching from mobile to desktop.
    Changed the play button

--- index.js
+++ index.js
@@ -3,27 +3,48 @@
 
 const navHamburgerIconToggle = document.getElementById('nav-hamburger-icon-toggle');
 const navContainer = document.querySelector('.nav-container');
+const navLinkWrapper = document.querySelector('.nav-link-wrapper ');
 
 navHamburgerIconToggle.addEventListener('click', () => {
     navHamburgerIconToggle.classList.toggle('active');
-    navContainer.classList.toggle('active');
+    // navContainer.classList.toggle('active');
+
+    navLinkWrapper.classList.toggle("show-links");
+
 })
 
 
 // cover video play and pause
 
+// const videoToPlay = document.getElementById("video-home-cover");
+// const videoButton = document.getElementById("video-button");
+
+// videoButton.addEventListener('click', playVideoOnClick);
+
+
+// function playVideoOnClick() {
+//     if (!videoToPlay.paused) {
+//         videoToPlay.pause();
+//         videoButton.innerHTML = "Play Video";
+//     } else {
+//         videoToPlay.play();
+//         videoButton.innerHTML = "Pause";
+//     }
+// }
+
+const btn = document.querySelector('.switch-btn');
+const video = document.querySelector('.video-container');
 const videoToPlay = document.getElementById("video-home-cover");
-const videoButton = document.getElementById("video-button");
 
-videoButton.addEventListener('click', playVideoOnClick);
-
-function playVideoOnClick() {
-    if (!videoToPlay.paused) {
-        videoToPlay.pause();
-        videoButton.innerHTML = "Play Video";
-    } else {
+btn.addEventListener('click', () => {
+    if (!btn.classList.contains('slide')) {
+        btn.classList.add('slide');
         videoToPlay.play();
-        videoButton.innerHTML = "Pause";
+    } else {
+        btn.classList.remove('slide');
+        videoToPlay.pause();
     }
-}
+})
+
+
 
