ebf0a45d1ab9861cf177abe041df6195346feb88
rossivk <rosica.kirkova@gmail.com>
2023-03-14 08:16:25 +0200

    new sections added

--- index.js
+++ index.js
@@ -44,7 +44,28 @@ btn.addEventListener('click', () => {
         btn.classList.remove('slide');
         videoToPlay.pause();
     }
-})
+});
+
+
+// carousel gallery
+
+const images = ["DSC02663.jpg", "DSC02664.jpg", "DSC02665.jpg", "DSC02666.jpg", "DSC02667.jpg", "DSC02670.jpg", "DSC02674.jpg", "DSC02687.jpg", "DSC02693.jpg", "DSC02696.jpg", "DSC02697.jpg"];
+
+const carousel = document.querySelector('.carousel');
+const interval = setInterval(function () {
+    startCarousel();
+}, 3000);
+
+let index = 1;
+function startCarousel() {
+    carousel.style.backgroundImage = `url(./assets/images/${images[index++]})`;
+    carousel.classList.remove('fade');
+    void carousel.offsetWidth;
+    carousel.classList.add('fade')
+    if (index > images.length - 1) {
+        index = 0;
+    }
+}
 
 
 
